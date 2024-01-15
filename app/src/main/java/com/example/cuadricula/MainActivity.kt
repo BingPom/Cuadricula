6eb7551554ce31dc7a9cb8b9ac297b5f537294ef
BingPom <bingpom@mailfence.com>
2024-01-15 14:08:59 +0100

    Commit

--- app/src/main/java/com/example/cuadricula/MainActivity.kt
+++ app/src/main/java/com/example/cuadricula/MainActivity.kt
@@ -7,11 +7,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -49,7 +46,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CuadriculaMain(modifier: Modifier = Modifier) {
+fun CuadriculaMain() {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = Modifier.fillMaxSize()
